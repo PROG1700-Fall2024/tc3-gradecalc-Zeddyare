1a2b4fd429707e29fe7539ab08d5a2f8a17dcf49
Zeddyare <w0433704@nscc.ca>
2024-10-21 13:21:12 -0300

    Finished and working

--- TC3-GradeCalc.py
+++ TC3-GradeCalc.py
@@ -17,13 +17,50 @@
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    #variable initializing
+    letterGrade=""
+    numGrade=0.0
+    modVal=0.3 
+    modifier=""
+    finGrade=0.0
 
+    #greeting print command and explanation
+    print("Grade Point Calculator\n")
+    print("""Valid letter grades that can be entered: A, B, C, D, F.
+Valid grade modifiers are +, - or nothing. 
+All letter grades except F can include a + or - symbol.
+Calculated grade point value cannot exceed 4.0\n
+""")
+    #input commands
+    letterGrade=input("Please enter a letter grade: ").lower()
+    modifier=input("Please enter a modifier (+, - or nothing): ") 
 
-
-
-
-
-
+    #if statements for conversion 
+    if letterGrade == "a":
+        numGrade=4.0 
+    elif letterGrade == "b":
+        numGrade=3.0
+    elif letterGrade == "c":
+        numGrade=2.0
+    elif letterGrade == "d":
+        numGrade=1.0
+    elif letterGrade == "f":
+        numGrade=0.0  
+    else: 
+        print("You entered an invalid letter grade.") 
+    
+    #math for grade output 
+    if (numGrade==4.0 and modifier=="+") or numGrade==0.0 or modifier=="":
+        finGrade=numGrade 
+    elif modifier=="+":
+        finGrade=numGrade+modVal
+    elif modifier=="-":
+        finGrade=numGrade-modVal
+    else:
+        print("Not a valid modifier.")
+    
+    #outout 
+    print("The numeric value is: {0:.1f}".format(finGrade))
     # YOUR CODE ENDS HERE
 
 main()
